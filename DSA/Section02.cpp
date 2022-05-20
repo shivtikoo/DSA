2d1c898eca3071173d32c8a12b7b5ab4d3240b5b
shiv <shiivvv.0023@gmail.com>
2022-05-20 10:53:37 +0530

    arrays

--- DSA/Section02.cpp
+++ DSA/Section02.cpp
@@ -4,10 +4,25 @@ using namespace std;
 int main(){
     //Arrays
     int arr[5];
+    //If no value given for certain index then the default value is 0
     for (int i=0;i<5;i++){
         arr[i]=i+1;
+        cout<<arr[i]<<endl;
+    }
+    //FOR EACH LOOP
+    for (int x:arr){
+        cout<<x<<endl;
     }
     cout<<sizeof(arr);
+    //each int 4 bytes so 20
+    cout<<"Enter size of arr:"<<endl;
+    int n;
+    cin>>n;
+   
+    int A[n];
+    for(int x:A){
+        cout<<x<<endl;
+    }
 
     return 0;
 
