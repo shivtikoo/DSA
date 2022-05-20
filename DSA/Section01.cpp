5912ea72f1f4538c6a5bf7c9973f12de17101252
shiv <shiivvv.0023@gmail.com>
2022-05-20 10:39:04 +0530

    extra files

---
+++ DSA/Section01.cpp
@@ -0,0 +1,5 @@
+//Without data instructions can't take place
+//How you place data inside memory; DATA STRUCTURES
+//Physical Data structures: Arrays, Matrices, Linked Lists
+//Logical Data structures: Stack, Queues, Trees, Graph, Hashing
+//Recursion uses stack internally, not good for large problems
---
+++ DSA/Section02.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+//C & C++ concepts
+int main(){
+    //Arrays
+    int arr[5];
+    for (int i=0;i<5;i++){
+        arr[i]=i+1;
+    }
+    cout<<sizeof(arr);
+
+    return 0;
+
+
+}
