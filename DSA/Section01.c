849c1321eb89bd92c87e8e771ebbebaa0dee45d2
shiv <shiivvv.0023@gmail.com>
2022-05-20 09:15:09 +0530

    Section01

---
+++ DSA/Section01.c
@@ -0,0 +1,4 @@
+//Without data instructions can't take place
+//How you place data inside memory; DATA STRUCTURES
+//Physical Data structures: Arrays, Matrices, Linked Lists
+//Logical Data structures: Stack, Queues, Trees, Graph, Hashing
