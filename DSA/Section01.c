cac7b63bfbf960c4a0984667a43eaeca43edf6f2
shiv <shiivvv.0023@gmail.com>
2022-05-20 10:13:38 +0530

    Section 01 complete

--- DSA/Section01.c
+++ DSA/Section01.c
@@ -2,3 +2,4 @@
 //How you place data inside memory; DATA STRUCTURES
 //Physical Data structures: Arrays, Matrices, Linked Lists
 //Logical Data structures: Stack, Queues, Trees, Graph, Hashing
+//Recursion uses stack internally, not good for large problems
