c61d2be458ae6b740b5ecfa8331851239bdfc91d
shivtikoo <shiivvv.0023@gmail.com>
2023-06-27 22:01:33 +0530

    27-06-2023

---
+++ LinkedList.cpp/CircularLinkedList.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+
+};
+
+//ONLY USE TAIL POINTER
+
+void insert(Node* &tail, int data){
+
+    Node* temp= new Node(data);
+
+    temp->next=tail->next;
+    tail->next=temp;
+
+    tail=temp;
+
+}
+
+void print(Node* tail){
+
+    Node*temp=tail;
+
+    while(temp->next!=tail){
+        
+        cout<<temp->data<<" ";
+        temp=temp->next;
+
+    }
+    //dp while loop os also an interesting concept
+    cout<<endl;
+
+
+}
+
+int main(){
+    
+    //last node ko simply point karao to first node
+    Node* tail= new Node(7);
+
+    insert(tail,11);
+    insert(tail,13);
+
+
+    print(tail);
+
+
+}
--- LinkedList.cpp/DoublyLinkedList.cpp
+++ LinkedList.cpp/DoublyLinkedList.cpp
@@ -37,10 +37,62 @@ int getlen(Node* head){
     return count;
 }
 
+void insert(Node* &head, int data){
+    Node* temp=new Node(data);
+
+    head->prev=temp;
+    temp->next=head;
+    head=temp;
+}
+
+void insertTail(Node* &tail, int data){
+    Node*temp=new Node(data);
+
+    tail->next=temp;
+    temp->prev=tail;
+    tail=temp;
+
+
+}
+
+void insertPos(Node* &head, int data, int pos){
+    Node* temp = new Node(data);
+    if(pos==1){
+        insert(head,data);
+    }
+
+    int count=1;
+    Node* prev = head;
+
+    while(count<pos-1){
+        prev=prev->next;
+        count++;
+    }
+    
+    temp->prev=prev;
+    temp->next=prev->next;
+    prev->next=temp;
+    prev->next->prev=temp;
+
+}
+
 int main(){
     Node* head= new Node(12);
+    Node* tail= head;
     print(head);
 
     cout<<getlen(head)<<endl;
+
+    insert(head,11);
+
+    print(head);
+
+    insertTail(tail,13);
+
+    print(head);
+
+    insertPos(head,14,3);
+
+    print(head);
 
 }
